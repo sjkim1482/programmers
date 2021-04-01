98d5069458924dd4224669ed9aeb1044e50be21d
sjkim1482 <monochrose94@gmail.com>
2021-04-01 11:45:12 +0900

    LEVEL 1 같은 숫자는 싫어

---
+++ src/main/java/programmers/NonEqualNumber.java
@@ -0,0 +1,53 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class NonEqualNumber {
+
+	public static void main(String[] args) {
+		/*배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+
+			arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+			arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+			배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+			
+			제한사항
+			배열 arr의 크기 : 1,000,000 이하의 자연수
+			배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수*/
+
+//		int[] arr = {1, 1, 3, 3, 0, 1, 1};
+		int[] arr = {4,4,4,3,3};
+		int[] notOverlap = new int[arr.length];
+		int count = 0;
+		for(int i = 0; i < notOverlap.length; i++){
+			notOverlap[i] = -1;
+		}
+		
+		for(int i = 0; i<notOverlap.length-1; i++){
+			if(arr[i]!=arr[i+1]){
+				notOverlap[count] = arr[i];
+				count++;
+			}
+		}
+		if(arr[arr.length-2]==arr[arr.length-1]){
+			notOverlap[count] = arr[arr.length-1];
+			count++;
+		}
+		if(arr[arr.length-2]!=arr[arr.length-1]){
+			notOverlap[count] = arr[arr.length-1];
+			count++;
+		}
+	
+		
+		int[] answer = new int[count];
+		
+		for(int i = 0; i<answer.length; i++){
+			answer[i] =notOverlap[i] ;
+		}
+		
+		System.out.println(Arrays.toString(answer));
+	
+		
+	}
+
+}
