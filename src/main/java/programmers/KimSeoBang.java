0f731b7e30fd772237e0f16f3696256865f2c836
sjkim1482 <monochrose94@gmail.com>
2021-04-01 12:08:29 +0900

    LEVEL 1 서울에서 김서방 찾기

---
+++ src/main/java/programmers/KimSeoBang.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class KimSeoBang {
+
+	public static void main(String[] args) {
+		/*String[] point = {"Jane", "Kim"};
+		String answer = "";
+		
+		for(int i = 0; i<point.length; i++){
+			if(point[i].equals("Kim")){
+				answer = "김서방은 "+i+"에 있다";
+			}
+		}
+		
+		System.out.println(answer);*/
+	
+	}
+	
+
+}
