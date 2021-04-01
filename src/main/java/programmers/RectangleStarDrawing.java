da390a8dbd189f0da05ff0c90da97abf0ebc3feb
sjkim1482 <monochrose94@gmail.com>
2021-04-01 12:51:28 +0900

    LEVEL 1 직사각형 별찍기

---
+++ src/main/java/programmers/RectangleStarDrawing.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class RectangleStarDrawing {
+
+	public static void main(String[] args) {
+		/*
+		 * 문제 설명 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다. 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인
+		 * 직사각형 형태를 출력해보세요.
+		 * 
+		 * 제한 조건 n과 m은 각각 1000 이하인 자연수입니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i = 0; i<b; i++){
+            for(int j = 0; j<a; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+
+}
